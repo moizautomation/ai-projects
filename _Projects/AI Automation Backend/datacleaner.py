a2affcbd95f894b280826edcb9a1af21df1960cd
Abdul Moiz <abdulmoiz00201@gmail.com>
2026-06-17 23:10:45 +0500

    Connected a database and inserted a table for saving files

--- _Projects/AI Automation Backend/datacleaner.py
+++ _Projects/AI Automation Backend/datacleaner.py
@@ -1,5 +1,10 @@
 from fastapi import FastAPI,UploadFile, File
 import pandas as pd
+import sqlite3
+
+conn = sqlite3.connect("database.db")
+
+cursor = conn.cursor()
 
 app = FastAPI()
 
@@ -42,9 +47,48 @@ async def clean(file : UploadFile = File(...)):
     #standardizing the columns name
     df.columns = df.columns.str.lower().str.replace(" ","_")
     
-    defi = df.head().to_dict(orient="records")
+    cleaned = df.head().to_dict(orient="records")
 
     return {
         "normal" : temp,
-        "cleaned" : defi
+        "cleaned" : cleaned
     }
+
+@app.post("/save-clean-data")
+async def save(file : UploadFile = File(...)):
+    df = pd.read_csv(file.file, encoding="latin1")
+
+    temp = df.head().to_dict(orient="records")
+
+    before = len(df)
+
+    #removing duplicates
+    df = df.drop_duplicates()
+
+    after = len(df)
+
+    for col in df.columns:
+        #to get the type of the column
+        if df[col].dtype == "object":
+            #placing 0 in missing columns
+            df[col] = df[col].fillna("Unknown")
+        else:
+            df[col] = df[col].fillna(0)
+
+    #standardizing the columns name
+    df.columns = df.columns.str.lower().str.replace(" ","_")
+    
+    cleaned = df.head().to_dict(orient="records")
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS cleaned_uploads(
+                   Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                   FileName TEXT,
+                   Original_rows INT,
+                   Cleaned_rows INT,
+                   Columns TEXT
+        )
+    """)
+    cursor.execute("""
+    INSERT INTO cleaned_uploads(Filename,Original_rows,Cleaned_rows,Columns) VALUES(:Filename,:Original_rows,:Cleaned_rows,:Columns)""",{"Filename" : File.filename, "Original_rows": before, "Cleaned_rows" : after, "Columns" : df.to_dict()})
+    conn.commit()
